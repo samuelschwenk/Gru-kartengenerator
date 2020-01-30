77f85e0517cc05b68996e67326513acf413a90de
smocktheripper <smocktheripper@users.noreply.github.com>
2020-01-30 20:37:24 +0100

    saving pics is possible

--- app/src/main/java/com/example/grukartengenerator/MainActivity.java
+++ app/src/main/java/com/example/grukartengenerator/MainActivity.java
@@ -18,32 +18,51 @@ import android.provider.MediaStore;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 import java.io.File;
 
 public class MainActivity extends AppCompatActivity {
     Button continueButton;
     File imageFile = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES), "foto.jpg");
+    RadioGroup maingroup;
+    RadioButton sonnig;
+    RadioButton regnerisch;
+    RadioButton frostig;
+    RadioButton bewoelkte;
+    EditText name;
+    EditText ort;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        maingroup = findViewById(R.id.maingroup);
+        sonnig = findViewById(R.id.sonnige);
+        regnerisch = findViewById(R.id.regnerische);
+        frostig = findViewById(R.id.frostige);
+        bewoelkte = findViewById(R.id.bewoelkte);
         continueButton = findViewById(R.id.continueButton);
+        name = findViewById(R.id.name);
+        ort = findViewById(R.id.ort);
+
         if (Build.VERSION.SDK_INT>= 23){
             requestPermissions(new String[]{Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.READ_EXTERNAL_STORAGE},1);
         }
-        continueButton.setOnClickListener(new View.OnClickListener(){
-            public  void onClick(View view){
-                StrictMode.VmPolicy.Builder builder = new StrictMode.VmPolicy.Builder();
-                StrictMode.setVmPolicy(builder.build());
-                Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(imageFile));
-                startActivityForResult(takePictureIntent,1);
+            continueButton.setOnClickListener(new View.OnClickListener() {
+                public void onClick(View view) {
+                    if(getWeatherstring(sonnig,regnerisch,frostig,bewoelkte,maingroup)!= null) {
+                    StrictMode.VmPolicy.Builder builder = new StrictMode.VmPolicy.Builder();
+                    StrictMode.setVmPolicy(builder.build());
+                    Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                    takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(imageFile));
+                    startActivityForResult(takePictureIntent, 1);
+                    }
+                }
+            });
 
-
-            }
-        });
     }
 
     @Override
@@ -61,6 +80,34 @@ public class MainActivity extends AppCompatActivity {
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         MediaScannerConnection.scanFile(getApplicationContext(), new String[]{imageFile.toString()}, null, null);
         Intent contentIntent = new Intent(getApplicationContext(), Result.class);
+        contentIntent.putExtra("wetter",getWeatherstring(sonnig,regnerisch,frostig,bewoelkte,maingroup));
+        contentIntent.putExtra("name",name.getText().toString());
+        contentIntent.putExtra("ort",ort.getText().toString());
         startActivity(contentIntent);
     }
+public String getWeatherstring(RadioButton sonnig, RadioButton regnerisch, RadioButton frostig, RadioButton bewoelkte, RadioGroup maingroup){
+        this.sonnig = sonnig;
+        this.regnerisch = regnerisch;
+        this.frostig = frostig;
+        this.bewoelkte = bewoelkte;
+        this.maingroup = maingroup;
+        String Ausgabestring;
+        if(maingroup.getCheckedRadioButtonId() == sonnig.getId()){
+            Ausgabestring = "sonnigen";
+            return  Ausgabestring;
+        }
+    if(maingroup.getCheckedRadioButtonId() == regnerisch.getId()){
+        Ausgabestring = "regnerischen";
+        return  Ausgabestring;
+    }
+    if(maingroup.getCheckedRadioButtonId() == frostig.getId()){
+        Ausgabestring = "frostigen";
+        return  Ausgabestring;
+    }
+    if(maingroup.getCheckedRadioButtonId() == bewoelkte.getId()){
+        Ausgabestring = "bewölkten";
+        return  Ausgabestring;
+    }
+    return "";
+}
 }
--- app/src/main/java/com/example/grukartengenerator/Result.java
+++ app/src/main/java/com/example/grukartengenerator/Result.java
@@ -1,42 +1,149 @@
 package com.example.grukartengenerator;
 
 import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.drawable.BitmapDrawable;
 import android.media.Image;
 import android.media.MediaScannerConnection;
+import android.net.Uri;
+import android.net.sip.SipSession;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
+import android.util.TypedValue;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CompoundButton;
 import android.widget.ImageView;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.jar.Attributes;
 
 public class Result extends AppCompatActivity {
     ImageView imageView;
     File imageFile = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES), "foto.jpg");
     File imageFile2 = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES), "grußkarte.jpg");
+    Button speichernButton ;
+    RadioButton black;
+    RadioButton white;
+    RadioButton red;
+    RadioButton blue;
+    RadioButton green;
+    RadioGroup group;
+    String Wetter;
+    String Ort;
+    String Name;
+    Intent oldIntent;
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result);
+        speichernButton = findViewById(R.id.speichernButton);
         imageView = findViewById(R.id.image);
-        setImage();
+        black = findViewById(R.id.blackbutton);
+        white = findViewById(R.id.whitebutton);
+        red = findViewById(R.id.redbutton);
+        blue = findViewById(R.id.bluebutton);
+        green = findViewById(R.id.greenbutton);
+        group = findViewById(R.id.group);
+        oldIntent = getIntent();
+        Ort = oldIntent.getStringExtra("ort");
+        Name = oldIntent.getStringExtra("name");
+        Wetter = oldIntent.getStringExtra("wetter");
+        setImage(R.color.black);
+        group.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup radioGroup, int i) {
+                setImage(getColor(black,white,red,blue,green,group));
+            }
+        });
+        speichernButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                produceGrueßkarte();
+            }
+        });
     }
-    public void setImage(){
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    public void setImage(int Color){
         Bitmap bitmap = BitmapFactory.decodeFile(imageFile.toString());
         Bitmap mutableBitmap = bitmap.copy(Bitmap.Config.ARGB_8888,true);
         Canvas canvas = new Canvas(mutableBitmap);
         Paint paint = new Paint();
-        paint.setColor(Color.BLACK);
-        paint.setTextSize(100);
-        canvas.drawText("Testtext", 10,200,paint);
+        paint.setColor(Color);
+        paint.setTextSize(50);
+        canvas.drawText("Viele Grüße aus dem "+Wetter+" "+Ort, 10,200,paint);
+        canvas.drawText("Viele Grüße, "+Name, 10,280,paint);
         imageView.setImageBitmap(mutableBitmap);
     }
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    public int getColor(RadioButton black, RadioButton white, RadioButton red, RadioButton blue, RadioButton green, RadioGroup group){
+        this.black = black;
+        this.white = white;
+        this.red = red;
+        this.blue = blue;
+        this.green = green;
+        this.group = group;
+        int color;
+        if(group.getCheckedRadioButtonId()== black.getId()){
+            color = getColor(R.color.black);
+            return color;
+        }
+        if(group.getCheckedRadioButtonId()== white.getId()){
+            color = getColor(R.color.white);
+            return color;
+        }
+        if(group.getCheckedRadioButtonId()== red.getId()){
+            color = getColor(R.color.red);
+            return color;
+        }
+        if(group.getCheckedRadioButtonId()== blue.getId()){
+            color = getColor(R.color.blue);
+            return color;
+        }
+        if(group.getCheckedRadioButtonId()== green.getId()){
+            color = getColor(R.color.green);
+            return color;
+        }
+        return 0;
+    }
+    public void produceGrueßkarte(){
+        imageView.buildDrawingCache();
+        Bitmap bmp = imageView.getDrawingCache();
 
+        try{
+            FileOutputStream fos = new FileOutputStream(imageFile2);
+            bmp.compress(Bitmap.CompressFormat.JPEG, 100, fos);
+            fos.close();
+
+            scanFile(getBaseContext(), Uri.fromFile(imageFile2));
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    private void scanFile(Context context , Uri imageUri){
+        Intent scanIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
+        scanIntent.setData(imageUri);
+        context.sendBroadcast(scanIntent);
+
+    }
 }
